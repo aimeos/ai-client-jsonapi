a107958988ddbf0bc95152563eb53c5958a790a4
Aimeos <aimeos@aimeos.org>
2017-02-25 14:26:23 +0100

    Implemented JSON API for stock domain

---
+++ client/jsonapi/templates/stock/get-default.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2017
+ * @package Client
+ * @subpackage JsonApi
+ */
+
+
+$target = $this->config( 'client/jsonapi/url/target' );
+$cntl = $this->config( 'client/jsonapi/url/controller', 'jsonapi' );
+$action = $this->config( 'client/jsonapi/url/action', 'get' );
+$config = $this->config( 'client/jsonapi/url/config', array() );
+
+
+$view = $this;
+$enc = $this->encoder();
+
+
+$ref = array( 'id', 'resource', 'filter', 'page', 'sort', 'include', 'fields' );
+$params = array_intersect_key( $this->param(), array_flip( $ref ) );
+
+
+$total = $this->get( 'total', 0 );
+$offset = max( $this->param( 'page/offset', 0 ), 0 );
+$limit = max( $this->param( 'page/limit', 100 ), 1 );
+
+$first = ( $offset > 0 ? 0 : null );
+$prev = ( $offset - $limit >= 0 ? $offset - $limit : null );
+$next = ( $offset + $limit < $total ? $offset + $limit : null );
+$last = ( ((int) ($total / $limit)) * $limit > $offset ? ((int) ($total / $limit)) * $limit : null );
+
+
+$fields = $this->param( 'fields', array() );
+
+foreach( (array) $fields as $resource => $list ) {
+	$fields[$resource] = array_flip( explode( ',', $list ) );
+}
+
+
+$entryFcn = function( \Aimeos\MShop\Stock\Item\Iface $item ) use ( $fields, $view, $target, $cntl, $action, $config )
+{
+	$attributes = $item->toArray();
+	$type = $item->getResourceType();
+	$params = array( 'resource' => $type, 'id' => $item->getId() );
+
+	if( isset( $fields[$type] ) ) {
+		$attributes = array_intersect_key( $attributes, $fields[$type] );
+	}
+
+	$entry = array(
+		'id' => $item->getId(),
+		'type' => $item->getResourceType(),
+		'links' => array(
+			'self' => array(
+				'href' => $view->url( $target, $cntl, $action, $params, array(), $config ),
+				'allow' => array( 'GET' ),
+			),
+		),
+		'attributes' => $attributes,
+	);
+
+	return $entry;
+};
+
+
+?>
+{
+	"meta": {
+		"total": <?php echo $total; ?>
+
+	},
+
+	"links": {
+		<?php if( is_array( $this->get( 'items' ) ) ) : ?>
+			<?php if( $first !== null ) : ?>
+				"first": "<?php $params['page']['offset'] = $first; echo $this->url( $target, $cntl, $action, $params, array(), $config ); ?>",
+			<?php endif; ?>
+			<?php if( $prev !== null ) : ?>
+				"prev": "<?php $params['page']['offset'] = $prev; echo $this->url( $target, $cntl, $action, $params, array(), $config ); ?>",
+			<?php endif; ?>
+			<?php if( $next !== null ) : ?>
+				"next": "<?php $params['page']['offset'] = $next; echo $this->url( $target, $cntl, $action, $params, array(), $config ); ?>",
+			<?php endif; ?>
+			<?php if( $last !== null ) : ?>
+				"last": "<?php $params['page']['offset'] = $last; echo $this->url( $target, $cntl, $action, $params, array(), $config ); ?>",
+			<?php endif; ?>
+		<?php endif; ?>
+		"self": "<?php $params['page']['offset'] = $offset; echo $this->url( $target, $cntl, $action, $params, array(), $config ); ?>"
+	},
+
+	<?php if( isset( $this->errors ) ) : ?>
+
+		"errors": <?php echo json_encode( $this->errors, JSON_PRETTY_PRINT ); ?>
+
+	<?php elseif( isset( $this->items ) ) : ?>
+
+		<?php
+			$data = $included = array();
+			$items = $this->get( 'items', array() );
+
+			if( is_array( $items ) )
+			{
+				foreach( $items as $stockId => $stockItem ) {
+					$data[] = $entryFcn( $stockItem );
+				}
+			}
+			else
+			{
+				$data = $entryFcn( $items );
+			}
+		 ?>
+
+		"data": <?php echo json_encode( $data ); ?>
+
+	<?php endif; ?>
+
+}
---
+++ client/jsonapi/tests/Client/JsonApi/Stock/StandardTest.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2017
+ */
+
+
+namespace Aimeos\Client\JsonApi\Stock;
+
+
+class StandardTest extends \PHPUnit_Framework_TestCase
+{
+	private $context;
+	private $object;
+	private $view;
+
+
+	protected function setUp()
+	{
+		$this->context = \TestHelperJapi::getContext();
+		$templatePaths = \TestHelperJapi::getTemplatePaths();
+		$this->view = $this->context->getView();
+
+		$this->object = new \Aimeos\Client\JsonApi\Stock\Standard( $this->context, $this->view, $templatePaths, 'stock' );
+	}
+
+
+	public function testGetItem()
+	{
+		$stockManager = \Aimeos\MShop\Factory::createManager( $this->context, 'stock' );
+		$stockId = $stockManager->findItem( 'CNE', [], 'product', 'default' )->getId();
+
+		$params = array(
+			'id' => $stockId,
+			'fields' => array(
+				'stock' => 'stock.id,stock.productcode,stock.stocklevel'
+			),
+			'sort' => 'stock.id'
+		);
+
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $params );
+		$this->view->addHelper( 'param', $helper );
+
+		$response = $this->object->get( $this->view->request(), $this->view->response() );
+		$result = json_decode( (string) $response->getBody(), true );
+
+		$this->assertEquals( 200, $response->getStatusCode() );
+		$this->assertEquals( 1, count( $response->getHeader( 'Allow' ) ) );
+		$this->assertEquals( 1, count( $response->getHeader( 'Content-Type' ) ) );
+
+		$this->assertEquals( 1, $result['meta']['total'] );
+		$this->assertEquals( 'stock', $result['data']['type'] );
+		$this->assertEquals( 3, count( $result['data']['attributes'] ) );
+		$this->assertEquals( 'CNE', $result['data']['attributes']['stock.productcode'] );
+
+		$this->assertArrayNotHasKey( 'errors', $result );
+	}
+
+
+	public function testGetItems()
+	{
+		$params = array(
+			'filter' => array( 's_prodcode' => ['CNC', 'CNE'] ),
+			'sort' => 'stock.productcode',
+		);
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $params );
+		$this->view->addHelper( 'param', $helper );
+
+		$response = $this->object->get( $this->view->request(), $this->view->response() );
+		$result = json_decode( (string) $response->getBody(), true );
+
+		$this->assertEquals( 200, $response->getStatusCode() );
+		$this->assertEquals( 1, count( $response->getHeader( 'Allow' ) ) );
+		$this->assertEquals( 1, count( $response->getHeader( 'Content-Type' ) ) );
+
+		$this->assertEquals( 2, $result['meta']['total'] );
+		$this->assertEquals( 2, count( $result['data'] ) );
+		$this->assertEquals( 'stock', $result['data'][0]['type'] );
+
+		$this->assertArrayNotHasKey( 'errors', $result );
+	}
+}
