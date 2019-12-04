031891a27219a00340881ba3a0416234b6a92f11
Aimeos <aimeos@aimeos.org>
2019-12-04 16:06:18 +0100

    Support searching for catalog items

--- client/jsonapi/tests/Client/JsonApi/Catalog/StandardTest.php
+++ client/jsonapi/tests/Client/JsonApi/Catalog/StandardTest.php
@@ -65,10 +65,44 @@ class StandardTest extends \PHPUnit\Framework\TestCase
 	}
 
 
+	public function testGetItems()
+	{
+		$params = array(
+			'filter' => [
+				'==' => ['catalog.code' => ['cafe', 'tea']],
+			],
+			'fields' => array(
+				'catalog' => 'catalog.id,catalog.label'
+			),
+			'sort' => 'catalog.id',
+			'include' => 'media,text'
+		);
+
+		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $params );
+		$this->view->addHelper( 'param', $helper );
+
+		$response = $this->object->get( $this->view->request(), $this->view->response() );
+		$result = json_decode( (string) $response->getBody(), true );
+
+		$this->assertEquals( 200, $response->getStatusCode() );
+		$this->assertEquals( 1, count( $response->getHeader( 'Allow' ) ) );
+		$this->assertEquals( 1, count( $response->getHeader( 'Content-Type' ) ) );
+
+		$this->assertEquals( 2, $result['meta']['total'] );
+		$this->assertEquals( 'catalog', $result['data'][0]['type'] );
+		$this->assertEquals( 1, count( $result['data'][0]['relationships']['text']['data'] ) );
+		$this->assertEquals( 2, count( $result['data'][0]['relationships']['media']['data'] ) );
+		$this->assertEquals( 'catalog', $result['data'][1]['type'] );
+		$this->assertEquals( 5, count( $result['data'][1]['relationships']['text']['data'] ) );
+		$this->assertEquals( 9, count( $result['included'] ) );
+
+		$this->assertArrayNotHasKey( 'errors', $result );
+	}
+
+
 	public function testGetItemNoID()
 	{
 		$params = array(
-			'filter' => array( '>=' => array( 'catalog.level' => 0 ) ),
 			'include' => 'catalog,media'
 		);
 		$helper = new \Aimeos\MW\View\Helper\Param\Standard( $this->view, $params );
